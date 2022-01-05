ecb0367def9e223f87daac68019b72debcd8fe66
rossignolli <vitorrossignolli@gmail.com>
2022-01-04 22:17:33 -0300

    console logs and variables prints

--- tripleX.cpp
+++ tripleX.cpp
@@ -2,5 +2,10 @@
 
 int main()
 {
+    int a = 0;
+    std::cout << "You are a secret agent breaking into a secure server room.\n";
+    std::cout << "Enter the correct code to continue...\n";
+    std::cout << "This variable contains the value " << a << "\n";
+
     return 0;
 }
